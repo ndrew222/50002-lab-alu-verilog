dc10b13254b110820317f616e7b137a54b87929b
ndrew222 <38881000+ndrew222@users.noreply.github.com>
2025-11-25 23:22:19 +0800

    Change alufn to alufn0 because Prof Nat said so

--- source/alu.v
+++ source/alu.v
@@ -7,6 +7,15 @@ module alu (
     output        v,
     output        n
 );
+    adder adder (
+        .a(a),
+        .b(b),
+        .alufn0(alufn[0]),
+        .out(out),
+        .z(z),
+        .v(v),
+        .n(n)
+    );
 
     always @(*) begin
         // implement alu logic here
